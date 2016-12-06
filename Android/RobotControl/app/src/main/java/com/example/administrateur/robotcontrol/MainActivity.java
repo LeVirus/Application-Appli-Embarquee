9c05d6f3cbc9db49735a1d7fcd3b1ec35d517c6d
administrateur <administrateur@ge-elec.univ-avignon.fr>
2016-12-06 11:07:39 +0100

    ajout projet android

---
+++ Android/RobotControl/app/src/main/java/com/example/administrateur/robotcontrol/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.administrateur.robotcontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import de.tavendo.autobahn.WebSocketConnection;
+import de.tavendo.autobahn.WebSocketException;
+import de.tavendo.autobahn.WebSocketHandler;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    private static final String TAG = "de.tavendo.test1";
+    private final WebSocketConnection mConnection = new WebSocketConnection();
+    private static final String ip = "192.168.1.132:9000";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        start();
+    }
+
+    private void start() {
+
+        final String wsuri = "ws://"+ip;
+
+        try {
+            mConnection.connect(wsuri, new WebSocketHandler() {
+
+                @Override
+                public void onOpen() {
+                    Log.d(TAG, "Status: Connected to " + wsuri);
+                    mConnection.sendTextMessage("Hello, world!");
+                }
+
+                @Override
+                public void onTextMessage(String payload) {
+                    Log.d(TAG, "Got echo: " + payload);
+                }
+
+                @Override
+                public void onClose(int code, String reason) {
+                    Log.d(TAG, "Connection lost.");
+                }
+            });
+        } catch (WebSocketException e) {
+
+            Log.d(TAG, e.toString());
+        }
+    }
+
+}
