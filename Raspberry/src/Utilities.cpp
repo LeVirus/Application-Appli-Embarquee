1a9376b13d0acd8fefa52b0df57120da71b2896e
Ariel Roman <Ariel Roman>
2016-12-27 16:41:20 +0100

    Utilities.cpp debug

--- Raspberry/src/Utilities.cpp
+++ Raspberry/src/Utilities.cpp
@@ -5,13 +5,19 @@ std::vector<std::string> Utilities::split(std::string s, std::string delimiter)
 	std::cout << "Utilities Split" << std::endl;
 	std::vector<std::string> v;
 	size_t pos = 0;
-	size_t posPrec = 0;
 	int delimiterSize = delimiter.length();
+	size_t posPrec = 0-delimiterSize;
 	std::cout << "Split While begin" << std::endl;
 	while ((pos = s.find(delimiter,pos)) != std::string::npos) {
-		v.push_back(s.substr(posPrec+delimiterSize, pos));
+		std::cout << "Split While step 1, pos = " << pos << std::endl;
+		if (posPrec+delimiterSize < pos) {
+			std::cout << "Split While step 2, pos = " << pos << std::endl;
+			v.push_back(s.substr(posPrec+delimiterSize, pos));
+			std::cout << "Split While step 3, pos = " << pos << std::endl;
+		}
+		posPrec = pos;
 	}
 	std::cout << "Split While end" << std::endl;
-	v.push_back(s.substr(posPrec, s.length()-1));
+	v.push_back(s.substr(posPrec+delimiterSize, s.length()-1));
 	return v;
 }
