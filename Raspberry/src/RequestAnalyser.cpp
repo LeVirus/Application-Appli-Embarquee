ccb1b1a6a5a87dc32707d57aa34689308f121612
Ariel Roman <Ariel Roman>
2016-12-27 17:22:17 +0100

    RequestAnalyser.cpp debug

--- Raspberry/src/RequestAnalyser.cpp
+++ Raspberry/src/RequestAnalyser.cpp
@@ -3,7 +3,9 @@
 void RequestAnalyser::analyse(string request)
 {
 	vector<string> requestSplitted = Utilities::split(request,";");
+	std::cout << "SPLITTED 0 = " << requestSplitted.at(0)  << "END" << std::endl;
 	if (requestSplitted.at(0) == "movement") {
+		std::cout << "SPLITTED 1 = " << requestSplitted.at(1)  << "END" << std::endl;
 		if (requestSplitted.at(1) == "move") {
 			std::cout << "Move RequestAnalyser" << std::endl;
 			RobotControl::move();
